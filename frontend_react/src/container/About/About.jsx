463325cf21cccada211508bcf02f5ee9eeed0158
Alexandr <caiiiia_podoplelov@mail.ru>
2022-06-21 18:58:32 +0300

    sanity connects react

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -2,17 +2,21 @@ import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 
 import './about.scss';
-import { images } from '../../constants';
 
+import { urlFor, client } from '../../client';
 
-const abouts = [
-  { title: 'Web development', description: 'I am a good web developer', imgUrl: images.about01 },
-  { title: 'Web development', description: 'I am a good web developer', imgUrl: images.about02 },
-  { title: 'Web development', description: 'I am a good web developer', imgUrl: images.about03 },
-  { title: 'Web development', description: 'I am a good web developer', imgUrl: images.about04 }
-]
 
 const About = () => {
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query)
+      .then((res) => setAbouts(res));
+  }, []);
+
+
   return (
     <>
       <h2 className="head-text">
@@ -28,7 +32,7 @@ const About = () => {
               transition={{ duration: .5, type: 'tween' }}
               className='app__profile-item'
               key={ about.title + i }>
-                <img src={ about.imgUrl } alt={ about.title } />
+                <img src={ urlFor(about.imgUrl) } alt={ about.title } />
                 <h2 className="bold-text" style={{ marginTop: 20 }}>{ about.title }</h2>
                 <p className="p-text" style={{ marginTop: 10 }}>{ about.description }</p>
             </motion.div>
