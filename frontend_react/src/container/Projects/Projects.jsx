ea2922382fbe97e0688d1aa11a4735885d930a50
Alexandr <caiiiia_podoplelov@mail.ru>
2022-06-26 19:34:30 +0300

    finish

--- frontend_react/src/container/Projects/Projects.jsx
+++ frontend_react/src/container/Projects/Projects.jsx
@@ -77,15 +77,18 @@ const Projects = () => {
                     transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
                     className="app__work-hover app__flex">
 
-                      <a href={ work.projectLink } target="_blank" rel="noreferrer">
-                        <motion.div
-                          whileInView={{ scale: [0, 1] }}
-                          whileHover={{ scale: [1, 0.90] }}
-                          transition={{ duration: 0.25 }}
-                          className="app__flex"
-                        >
-                          <AiFillEye />
-                        </motion.div>
+                      <a
+                        href={ work.projectLink }
+                        target="_blank"
+                        rel="noreferrer">
+                          <motion.div
+                            whileInView={{ scale: [0, 1] }}
+                            whileHover={{ scale: [1, 0.90] }}
+                            transition={{ duration: 0.25 }}
+                            className="app__flex"
+                          >
+                            <AiFillEye />
+                          </motion.div>
                       </a>
 
                       <a href={ work.codeLink } target="_blank" rel="noreferrer">
